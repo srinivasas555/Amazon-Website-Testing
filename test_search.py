17b4ccfed8d1a8754608be557622c32260e6be55
srinivasa s <srini7312@gmail.com>
2025-05-26 18:07:07 +0530

    test_search.py

--- test_search.py
+++ test_search.py
@@ -1,19 +1,17 @@
+# test_search.py
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
 import pytest
-import time
 
 def test_amazon_search():
     driver = webdriver.Chrome()
     driver.get("https://www.amazon.in")
-    driver.maximize_window()
-
-    search = driver.find_element(By.ID, "twotabsearchtextbox")
-    search.send_keys("watch")
-    search.send_keys(Keys.RETURN)
-    time.sleep(3)
-
-    assert "watch" in driver.title.lower()
+    
+    search_box = driver.find_element(By.ID, "twotabsearchtextbox")
+    search_box.send_keys("headphones")
+    
+    search_button = driver.find_element(By.ID, "nav-search-submit-button")
+    search_button.click()
+    
+    assert "headphones" in driver.title.lower()
     driver.quit()
